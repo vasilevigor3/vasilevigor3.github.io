2beab2507f3d01e41bf66b47b79dbe4b4aac47d3
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-11 00:11:58 +0400

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,5 +1,6 @@
 document.createElement('div');
 
+javascript:alert(1);
 var iframeDocument = frame.contentWindow.document;
 
 var formData = new FormData(iframeDocument.querySelector('form.passport-Domik-Form'))
