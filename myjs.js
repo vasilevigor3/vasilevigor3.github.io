8804638b793e20af762fe374993adb80a27ea10c
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-10 14:16:02 +0400

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,4 +1,6 @@
-var formData = new FormData(document.querySelector('form.passport-Domik-Form'))
+var iframeDocument = frame.contentWindow.document;
+
+var formData = new FormData(iframeDocument.querySelector('form.passport-Domik-Form'))
 for (const pair of formData.entries()) {
   alert(`${pair[0]}, ${pair[1]}`);
 }
