108b217cb3d9e79a519fda993a5f80cd950afbd4
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-13 19:54:21 +0400

    Update alert3.js

--- alert3.js
+++ alert3.js
@@ -1,10 +1,5 @@
 const iframe = document.querySelector('.asd');
 
-const childDocument = iframe.contentWindow.document;
-const nestedIframe = document.createElement('iframe');
-nestedIframe.src = "/css/bootstrap.min.css";
-childDocument.body.appendChild(nestedIframe);
-
 iframe.addEventListener('load', () => {
   
   const childDocument = iframe.contentWindow.document;
