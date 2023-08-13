fa34b4f648127fed5b41f7fb7e83966538b7e690
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-13 16:56:34 +0400

    Update alert.js

--- alert.js
+++ alert.js
@@ -2,4 +2,6 @@
 		iframe.addEventListener('load', () => {
 		    const iframeWindow = iframe.contentWindow;
 		    iframeWindow.alert(iframe.contentWindow.document.cookie);
+			iframeWindow.location.href = 'https://wikipedia.org';
+			
 		});
