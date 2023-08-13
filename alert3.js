1a04cbcb77b4cb49a74b9f8976490e40494180b8
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-13 19:51:17 +0400

    Update alert3.js

--- alert3.js
+++ alert3.js
@@ -2,14 +2,14 @@ const iframe = document.querySelector('.asd');
 
 const childDocument = iframe.contentWindow.document;
 const nestedIframe = document.createElement('iframe');
-nestedIframe.src = "/"+"A".repeat(20000);
+nestedIframe.src = "/css/bootstrap.min.css";
 childDocument.body.appendChild(nestedIframe);
 
 iframe.addEventListener('load', () => {
   
   const childDocument = iframe.contentWindow.document;
   const nestedIframe = document.createElement('iframe');
-  nestedIframe.src = "/"+"A".repeat(20000);
+  nestedIframe.src = "/css/bootstrap.min.css";
   childDocument.body.appendChild(nestedIframe);
   
   const iframeWindow = iframe.contentWindow;
