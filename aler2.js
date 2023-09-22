b6c1d6efd413ea6547a4c484ec4a608446e0a97d
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-09-22 10:59:06 +0400

    Delete aler2.js

--- aler2.js
+++
@@ -1,7 +0,0 @@
-
-const iframe = document.querySelector('.asd');
-iframe.addEventListener('load', () => {
-    const iframeWindow = iframe.contentWindow;
-    const documentIframeWindow = iframeWindow.document;    
-    iframeWindow.alert(iframe.contentWindow.document.cookie);		
-});
