b2b78705d669320e8432ba90d535cb752f3129da
vasilevigor3 <32871596+vasilevigor3@users.noreply.github.com>
2023-09-22 11:00:12 +0400

    Delete pop.js

--- pop.js
+++
@@ -1,5 +0,0 @@
-    var a = window.open('https://dash.readme.com/to/opensea?redirect=asd');
-    a.addEventListener('load', function() {
-  // This code will run once the webpage finishes loading.
-  alert('Page has finished loading!'); 
-});
